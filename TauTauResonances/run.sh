2be3a8f06b5ed95def1db266df1d8b0e9d3d4b1d
IzaakWN <iwn_@hotmail.com>
2017-11-24 14:34:05 +0100

    Improved even more

--- TauTauResonances/run.sh
+++ TauTauResonances/run.sh
@@ -1,19 +1,23 @@
 #! /bin/bash
 
 FILES=(
-    Signal.xml
-#     DYJets_M-10to50.xml
-#     DYJets_M-50.xml
-#     TT_TuneCUETP8M1.xml
-#     WJetsToLNu.xml
-#     SingleMuon.xml
-#     SingleMuon_G.xml
-#     SingleMuon_H_v3.xml
-#     SingleMuon_H_v2.xml
+#    Signal.xml
+#    SUSY.xml
+#    DYJets_M-10to50.xml
+    DYJets_M-50.xml
+#    VBF.xml
+#    TT_TuneCUETP8M1.xml
+#    WJetsToLNu.xml
+#    SingleMuon.xml
 )
 
-printf "\n\n\n\n\n\n\n\n"
-if make distclean && make; then
+RUN_ONLY=0
+while getopts s option; do
+    [[ ${option} = s ]] && RUN_ONLY=1;
+done
+
+   [[ $RUN_ONLY > 0 ]] || printf "\n\n\n\n\n\n\n"
+if [[ $RUN_ONLY > 0 ]] || ( make distclean && make ); then
     for f in ${FILES[@]}; do
         printf "\n\n\n>>> %s%s%s\n\n" "$(tput bold)$(tput setaf 2)" $f "$(tput sgr0)"
         sframe_main config/$f

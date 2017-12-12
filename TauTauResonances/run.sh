9588a2a38d95e504fdea56874caaa34143349aad
IzaakWN <iwn_@hotmail.com>
2017-12-12 10:26:16 +0100

    updated files and options

--- TauTauResonances/run.sh
+++ TauTauResonances/run.sh
@@ -4,9 +4,10 @@ FILES=(
 #    Signal.xml
 #    SUSY.xml
 #    DYJets_M-10to50.xml
-    DYJets_M-50.xml
+#    DYJets_M-50.xml
 #    VBF.xml
-#    TT_TuneCUETP8M1.xml
+    TT_TuneCUETP8M1.xml
+#    TTJets_DiLept.xml
 #    WJetsToLNu.xml
 #    SingleMuon.xml
 )
@@ -16,10 +17,10 @@ while getopts s option; do
     [[ ${option} = s ]] && RUN_ONLY=1;
 done
 
-   [[ $RUN_ONLY > 0 ]] || printf "\n\n\n\n\n\n\n"
+   [[ $RUN_ONLY > 0 ]] || printf "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
 if [[ $RUN_ONLY > 0 ]] || ( make distclean && make ); then
     for f in ${FILES[@]}; do
-        printf "\n\n\n>>> %s%s%s\n\n" "$(tput bold)$(tput setaf 2)" $f "$(tput sgr0)"
+        printf "\n\n\n\n\n\n\n\n\n\n\n>>> %s%s%s\n\n" "$(tput bold)$(tput setaf 2)" $f "$(tput sgr0)"
         sframe_main config/$f
     done
 fi

fa5942ceb134c801604d0efb784681b9ee09ba71
IzaakWN <iwn_@hotmail.com>
2018-05-09 17:29:10 +0200

    removed comments

--- BatchSubmissionEM/submitIzaak_TID.sh
+++ BatchSubmissionEM/submitIzaak_TID.sh
@@ -4,23 +4,16 @@ function peval { echo ">>> $@"; eval "$@"; }
 
 if [[ $1 == 1 ]]; then
   echo ">>> JTF"
-#   peval "python -u submitSFrame.py -j Background_TT_JTF1p10.py --nosandbox --useEnv | tee nohup/nohup_TT_JTF1p10.log"
-#   peval "python -u submitSFrame.py -j Background_ST_JTF0p90.py --nosandbox --useEnv | tee nohup/nohup_ST_JTF0p90.log"
-#   peval "python -u submitSFrame.py -j Background_ST_JTF1p10.py --nosandbox --useEnv | tee nohup/nohup_ST_JTF1p10.log"
-#   peval "python -u submitSFrame.py -j Background_TT_JTF1p15.py --nosandbox --useEnv | tee nohup/nohup_TT_JTF1p15.log"
-#   peval "python -u submitSFrame.py -j Background_TT_JTF0p85.py --nosandbox --useEnv | tee nohup/nohup_TT_JTF0p85.log"
-#   peval "python -u submitSFrame.py -j Background_ST_JTF1p15.py --nosandbox --useEnv | tee nohup/nohup_ST_JTF1p15.log"
-#   peval "python -u submitSFrame.py -j Background_ST_JTF0p85.py --nosandbox --useEnv | tee nohup/nohup_ST_JTF0p85.log"
-#   peval "python -u submitSFrame.py -j Background_TT_EES0p97.py --nosandbox --useEnv | tee nohup/nohup_TT_EES0p97.log"
-#   peval "python -u submitSFrame.py -j Background_ST_EES1p03.py --nosandbox --useEnv | tee nohup/nohup_ST_EES1p03.log"
+  peval "python -u submitSFrame.py -j JTF/Background_TT_JTF0p90.py --nosandbox --useEnv | tee nohup/nohup_TT_JTF0p90.log"
+  peval "python -u submitSFrame.py -j JTF/Background_TT_JTF1p10.py --nosandbox --useEnv | tee nohup/nohup_TT_JTF1p10.log"
+  peval "python -u submitSFrame.py -j JTF/Background_ST_JTF0p90.py --nosandbox --useEnv | tee nohup/nohup_ST_JTF0p90.log"
+  peval "python -u submitSFrame.py -j JTF/Background_ST_JTF1p10.py --nosandbox --useEnv | tee nohup/nohup_ST_JTF1p10.log"
 elif [[ $1 == 2 ]]; then
   echo ">>> EES"
-#   peval "python -u submitSFrame.py -j Background_TT_JTF0p90.py --nosandbox --useEnv | tee nohup/nohup_TT_JTF0p90.log"
-#   peval "python -u submitSFrame.py -j Background_ST_JTF1p10.py --nosandbox --useEnv | tee nohup/nohup_ST_JTF1p10.log"
-#   peval "python -u submitSFrame.py -j Background_TT_JTF0p85.py --nosandbox --useEnv | tee nohup/nohup_TT_JTF0p85.log"
-#   peval "python -u submitSFrame.py -j Background_ST_JTF1p15.py --nosandbox --useEnv | tee nohup/nohup_ST_JTF1p15.log"
-#   peval "python -u submitSFrame.py -j Background_TT_EES1p03.py --nosandbox --useEnv | tee nohup/nohup_TT_EES1p03.log"
-#   peval "python -u submitSFrame.py -j Background_ST_EES0p97.py --nosandbox --useEnv | tee nohup/nohup_ST_EES0p97.log"
+  peval "python -u submitSFrame.py -j JTF/Background_TT_EES0p97.py --nosandbox --useEnv | tee nohup/nohup_TT_EES0p97.log"
+  peval "python -u submitSFrame.py -j JTF/Background_TT_EES1p03.py --nosandbox --useEnv | tee nohup/nohup_TT_EES1p03.log"
+  peval "python -u submitSFrame.py -j JTF/Background_ST_EES0p97.py --nosandbox --useEnv | tee nohup/nohup_ST_EES0p97.log"
+  peval "python -u submitSFrame.py -j JTF/Background_ST_EES1p03.py --nosandbox --useEnv | tee nohup/nohup_ST_EES1p03.log"
 else
     echo ">>> Wrong input! Use 1-2."
 fi
